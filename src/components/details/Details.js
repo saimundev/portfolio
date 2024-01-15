24053449a6c9188dbd9e3ed23cdc80633efaa52a
saimundev <saimunshezan@gmail.com>
2024-01-15 22:05:26 +0600

    new project added

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import Navber from "../navber/Navber";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 import "./Details.css";
 
 const Details = () => {
@@ -19,7 +19,6 @@ const Details = () => {
   }, [id]);
 
   const findProject = data.find((project) => project.id === parseInt(id));
-  console.log(findProject);
 
   return (
     <Container>
@@ -29,14 +28,38 @@ const Details = () => {
         initial={{ opacity: 0, y: "100px" }}
         whileInView={{ opacity: 1, y: "0px" }}
         viewport={true}
-        className="mt-2"
+        className="mt-2 "
       >
-        <img src={findProject?.image} alt="" className="img-fluid mt-5" />
+        <div className="text-center">
+          <img
+            src={findProject?.image}
+            alt=""
+            className="img-fluid mt-5 mx-auto"
+          />
+        </div>
         <h2 className="details__heading text-center">{findProject?.name}</h2>
         <div className="details__btn-group">
-          <a href={findProject?.live_site} target="_blank" className="details__btn">Live Site</a>
-          <a href={findProject?.clint_link} target="_blank" className="details__btn">Clind Site Code Link</a>
-          <a href={findProject?.server_link} target="_blank" className="details__btn">Server Site Code Link</a>
+          <a
+            href={findProject?.live_site}
+            target="_blank"
+            className="details__btn"
+          >
+            Live Site Link
+          </a>
+          <a
+            href={findProject?.clint_link}
+            target="_blank"
+            className="details__btn"
+          >
+            client Site Code Link
+          </a>
+          <a
+            href={findProject?.server_link}
+            target="_blank"
+            className="details__btn"
+          >
+            Server Site Code Link
+          </a>
         </div>
         <div className="">
           <h4 className="details__tech-header">Technology Used</h4>
--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -28,26 +28,32 @@ const Project = () => {
           transition={{ duration: 0.5 }}
           initial={{ opacity: 0, y: "300px" }}
           whileInView={{ opacity: 1, y: "0px" }}
-          viewport={{once:true}}
+          viewport={{ once: true }}
         >
           <div className="text-center">
             <h1 className="skill__title">Projects 💻</h1>
           </div>
           <Row className="g-4">
             {project?.map((item) => (
-              <Col md={4} className="project__cart">
-                <div className="">
-                  <img src={item.image} alt="" className="img-fluid rounded" />
-                  <span className="project__name">
-                    <span className="bold">Name</span> :{item.name}
-                  </span>
-                  <span className="project__catgory">
-                    {" "}
-                    <span className="bold">Category</span>:{item.category}
-                  </span>
-                  <span className="project__tech">
-                    <span className="bold">Technology</span> :{item.teconlogy}
-                  </span>
+              <Col md={4} className="">
+                <div className="project__cart">
+                  <img
+                    src={item.image}
+                    alt=""
+                    className="img-fluid rounded"
+                    style={{ height: "200px", objectFit: "contain" }}
+                  />
+                  <div className="project__text">
+                    <span className="project__name">
+                      <span className="bold">Name</span> :{item.name}
+                    </span>
+                    <span className="project__catgory">
+                      <span className="bold">Category</span>:{item.category}
+                    </span>
+                    <span className="project__tech">
+                      <span className="bold">Technology</span> :{item.teconlogy}
+                    </span>
+                  </div>
                   <button
                     className="project__btn"
                     onClick={() => redirect(item.id)}
